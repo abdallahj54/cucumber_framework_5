e6850f3cbc244ca7b76d2ad43309d8d937d800ac
Abdallah Jaber <abdallahj708@gmail.com>
2023-02-05 17:51:07 -0600

    updated repo for 2.5.23

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -8,8 +8,9 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/resources/features"},
         glue = "steps",
-        tags = "@Regression", //Empty means everything runs @Smoke only runs smoke and @Regression only runs regression
-        dryRun = false
+        tags = "", //Empty means everything runs @Smoke only runs smoke and @Regression only runs regression
+        dryRun = true, //true, it doesn't run an automation, but it shows all undefined steps (false, it will run the automation)
+        monochrome = false
 )
 public class Runner {
 }
